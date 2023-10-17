18f85b9b5af9ccfec377652c902f90be668bce60
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-17 18:50:41 -0300

    first commit

---
+++ src/main/java/jcorreaw/stdjsonwebtoken/StdJsonWebTokenApplication.java
@@ -0,0 +1,13 @@
+package jcorreaw.stdjsonwebtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdJsonWebTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdJsonWebTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jcorreaw/stdjsonwebtoken/StdJsonWebTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package jcorreaw.stdjsonwebtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdJsonWebTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
